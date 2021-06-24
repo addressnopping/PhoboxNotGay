a906e17f519d83bd43ac5600729134df0d4e6ae8
Peterrr__ <pedrohenriquebm1108@gmail.com>
2021-06-24 10:07:04 -0300

    optimized

---
+++ src/main/java/me/earth/phobos/Phobos.java
@@ -0,0 +1,174 @@
+package me.earth.phobos;
+
+import me.earth.phobos.features.gui.custom.GuiCustomMainScreen;
+import me.earth.phobos.features.modules.client.IRC;
+import me.earth.phobos.features.modules.misc.RPC;
+import me.earth.phobos.manager.*;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.lwjgl.opengl.Display;
+
+import java.io.IOException;
+
+@Mod(modid = "phobox", name = "PhoboxNotGay", version = "1.0")
+public class Phobos {
+    public static final String MODID = "phobox";
+    public static final String MODNAME = "PhoboxNotGay";
+    public static final String MODVER = "1.0";
+    public static final String NAME_UNICODE = "PhoboxNotGay";
+    public static final String PHOBOS_UNICODE = "PhoboX";
+    public static final String CHAT_SUFFIX = " \u23d0 PhoboxNotGay";
+    public static final String PHOBOS_SUFFIX = " \u23d0 PhoboX";
+    public static final Logger LOGGER = LogManager.getLogger("PhoboxNotGay");
+    public static ModuleManager moduleManager;
+    public static SpeedManager speedManager;
+    public static PositionManager positionManager;
+    public static RotationManager rotationManager;
+    public static CommandManager commandManager;
+    public static EventManager eventManager;
+    public static ConfigManager configManager;
+    public static FileManager fileManager;
+    public static FriendManager friendManager;
+    public static TextManager textManager;
+    public static ColorManager colorManager;
+    public static ServerManager serverManager;
+    public static PotionManager potionManager;
+    public static InventoryManager inventoryManager;
+    public static TimerManager timerManager;
+    public static PacketManager packetManager;
+    public static ReloadManager reloadManager;
+    public static TotemPopManager totemPopManager;
+    public static HoleManager holeManager;
+    public static NotificationManager notificationManager;
+    public static SafetyManager safetyManager;
+    public static GuiCustomMainScreen customMainScreen;
+    public static CosmeticsManager cosmeticsManager;
+    public static NoStopManager baritoneManager;
+    public static WaypointManager waypointManager;
+    @Mod.Instance
+    public static Phobos INSTANCE;
+    private static boolean unloaded;
+
+    static {
+        unloaded = false;
+    }
+
+    public static void load() {
+        LOGGER.info("\n\nLoading PhoboxNotGay 1.0");
+        unloaded = false;
+        if (reloadManager != null) {
+            reloadManager.unload();
+            reloadManager = null;
+        }
+        baritoneManager = new NoStopManager();
+        totemPopManager = new TotemPopManager();
+        timerManager = new TimerManager();
+        packetManager = new PacketManager();
+        serverManager = new ServerManager();
+        colorManager = new ColorManager();
+        textManager = new TextManager();
+        moduleManager = new ModuleManager();
+        speedManager = new SpeedManager();
+        rotationManager = new RotationManager();
+        positionManager = new PositionManager();
+        commandManager = new CommandManager();
+        eventManager = new EventManager();
+        configManager = new ConfigManager();
+        fileManager = new FileManager();
+        friendManager = new FriendManager();
+        potionManager = new PotionManager();
+        inventoryManager = new InventoryManager();
+        holeManager = new HoleManager();
+        notificationManager = new NotificationManager();
+        safetyManager = new SafetyManager();
+        waypointManager = new WaypointManager();
+        LOGGER.info("Initialized Managers");
+        moduleManager.init();
+        LOGGER.info("Modules loaded.");
+        configManager.init();
+        eventManager.init();
+        LOGGER.info("EventManager loaded.");
+        textManager.init(true);
+        moduleManager.onLoad();
+        totemPopManager.init();
+        timerManager.init();
+        if (moduleManager.getModuleByClass(RPC.class).isEnabled()) {
+            DiscordPresence.start();
+        }
+        cosmeticsManager = new CosmeticsManager();
+        LOGGER.info("PhoboxNotGay initialized!\n");
+    }
+
+    public static void unload(boolean unload) {
+        LOGGER.info("\n\nUnloading PhoboxNotGay 1.0");
+        if (unload) {
+            reloadManager = new ReloadManager();
+            reloadManager.init(commandManager != null ? commandManager.getPrefix() : ".");
+        }
+        if (baritoneManager != null) {
+            baritoneManager.stop();
+        }
+        Phobos.onUnload();
+        eventManager = null;
+        holeManager = null;
+        timerManager = null;
+        moduleManager = null;
+        totemPopManager = null;
+        serverManager = null;
+        colorManager = null;
+        textManager = null;
+        speedManager = null;
+        rotationManager = null;
+        positionManager = null;
+        commandManager = null;
+        configManager = null;
+        fileManager = null;
+        friendManager = null;
+        potionManager = null;
+        inventoryManager = null;
+        notificationManager = null;
+        safetyManager = null;
+        LOGGER.info("PhoboxNotGay unloaded!\n");
+    }
+
+    public static void reload() {
+        Phobos.unload(false);
+        Phobos.load();
+    }
+
+    public static void onUnload() {
+        if (!unloaded) {
+            try {
+                IRC.INSTANCE.disconnect();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            eventManager.onUnload();
+            moduleManager.onUnload();
+            configManager.saveConfig(Phobos.configManager.config.replaceFirst("phobos/", ""));
+            moduleManager.onUnloadPost();
+            timerManager.unload();
+            unloaded = true;
+        }
+    }
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        LOGGER.info("ohare is cute!!!");
+        LOGGER.info("faggot above - 3vt");
+        LOGGER.info("megyn wins again");
+        LOGGER.info("gtfo my logs - 3arth");
+        LOGGER.info("not gay anymore - Peterrr__");
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+        customMainScreen = new GuiCustomMainScreen();
+        Display.setTitle("PhoboxNotGay - v.1.0");
+        Phobos.load();
+    }
+}
+
